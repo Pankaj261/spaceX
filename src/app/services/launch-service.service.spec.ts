6b2b41a3e0d30c9d4c325a18b6a4f7f5d2a565e4
Pankaj <pankaj.upreti261@gmail.com>
2020-09-10 12:36:05 +0530

    add api url in enviroment.prod.ts

--- src/app/services/launch-service.service.spec.ts
+++ src/app/services/launch-service.service.spec.ts
@@ -1,16 +1,26 @@
-import { TestBed } from '@angular/core/testing';
-
+import { TestBed, inject } from '@angular/core/testing';
 import { LaunchServiceService } from './launch-service.service';
+import { HttpClient } from '@angular/common/http';
+import { spies } from './launch-service.mock';
 
 describe('LaunchServiceService', () => {
   let service: LaunchServiceService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LaunchServiceService);
+    TestBed.configureTestingModule({
+      providers: [
+        LaunchServiceService,
+        { provide: HttpClient, useValue: spies.HttpClientSpy }
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    service = TestBed.get(LaunchServiceService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
 });
